790bc949b3d6bdb0cb1d1ebcbf882ea858160b7c
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2023-03-09 12:27:28 -0500

    Add tasks

--- src/main/java/frc/robot/Platform/Tasks.java
+++ src/main/java/frc/robot/Platform/Tasks.java
@@ -1,5 +1,12 @@
 package frc.robot.Platform;
 
+import frc.robot.Tasks.DifferentialDrive;
+import frc.robot.Framework.IPeriodicTask;
+
 public class Tasks {
-    
+    static DifferentialDrive differentialDrive = new DifferentialDrive();
+
+    public static IPeriodicTask[] teleopTasks = {
+        (IPeriodicTask)differentialDrive
+    };
 }
