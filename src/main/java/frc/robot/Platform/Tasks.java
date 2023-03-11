fdd997c032ed9a4f6a11f538a283102470186e1e
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2023-03-11 14:55:37 -0500

    Add ArmSlide to tasks

--- src/main/java/frc/robot/Platform/Tasks.java
+++ src/main/java/frc/robot/Platform/Tasks.java
@@ -1,12 +1,14 @@
 package frc.robot.Platform;
 
-import frc.robot.Tasks.DifferentialDrive;
+import frc.robot.Tasks.*;
 import frc.robot.Framework.IPeriodicTask;
 
 public class Tasks {
     static DifferentialDrive differentialDrive = new DifferentialDrive();
+    static ArmSlide armSlide = new ArmSlide();
 
     public static IPeriodicTask[] teleopTasks = {
-        (IPeriodicTask)differentialDrive
+        (IPeriodicTask)differentialDrive,
+        (IPeriodicTask)armSlide
     };
 }
