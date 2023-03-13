c437b2c0e66993e5af64731c12941abcfc1c6d3a
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2023-03-13 16:27:21 -0400

    Initialize requestedArmPosition

--- src/main/java/frc/robot/Platform/Globals.java
+++ src/main/java/frc/robot/Platform/Globals.java
@@ -2,7 +2,7 @@ package frc.robot.Platform;
 
 public class Globals {
     public static boolean armAutomation = true;
-    public static ArmPosition requestedArmPosition;
+    public static ArmPosition requestedArmPosition = ArmPosition.park;
     public static class ArmConstraints {
         public static boolean liftClawPlexiglassCrash;
         public static boolean liftClawTiltCrash;
